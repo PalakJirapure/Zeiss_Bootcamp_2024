2d0acc2109eceac2728cfa5b774705a0f7e18145
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 13:07:00 +0530

    Update Device.cs

--- cSharpAssignments/UpdatedAssignment1/Device.cs
+++ cSharpAssignments/UpdatedAssignment1/Device.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
 
-class Device
+namespace DeviceValidatorAssignment
 {
-    [Required]
-    public string Id { get; set; }
+    class Device
+    {
+        [Required(ErrorMessage = "ID Property Requires Value")]
+        [Pattern(@"^\d{5}[A-Z]{3}$", ErrorMessage = "ID must be in the format 5 digits followed by 3 uppercase letters.")]
+        public string Id { get; set; }
 
-    [Range(10, 100)]
-    public int Code { get; set; }
+        [Range(10, 100, ErrorMessage = "Code Value Must Be Within 10-100")]
+        public int Code { get; set; }
 
-    [MaxLength(100)]
-    public string Description { get; set; }
-}
+        [MaxLength(100, ErrorMessage = "Max of 100 Characters are allowed")]
+        public string Description { get; set; }
+
+        [CustomValidation("ABC", ErrorMessage = "CustomProperty must contain the parameter ABC.")]
+        public string CustomProperty { get; set; }
+    }

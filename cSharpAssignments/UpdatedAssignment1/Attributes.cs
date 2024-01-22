2e6b1d0ec88d21c7c8523f36a6ff9b7a54f57b7c
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 13:07:34 +0530

    Update Attributes.cs

--- cSharpAssignments/UpdatedAssignment1/Attributes.cs
+++ cSharpAssignments/UpdatedAssignment1/Attributes.cs
@@ -1,30 +1,106 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
-[AttributeUsage(AttributeTargets.Property)]
-public class RequiredAttribute : Attribute
+namespace DeviceValidatorAssignment
 {
-}
-
-[AttributeUsage(AttributeTargets.Property)]
-public class RangeAttribute : Attribute
-{
-    public int Minimum { get; }
-    public int Maximum { get; }
-
-    public RangeAttribute(int minimum, int maximum)
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredAttribute : ValidationAttribute
     {
-        Minimum = minimum;
-        Maximum = maximum;
+        public override bool IsValid(object value)
+        {
+            return value != null && !string.IsNullOrWhiteSpace(value.ToString());
+        }
     }
-}
 
-[AttributeUsage(AttributeTargets.Property)]
-public class MaxLengthAttribute : Attribute
-{
-    public int MaxLength { get; }
-
-    public MaxLengthAttribute(int maxLength)
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RangeAttribute : ValidationAttribute
     {
-        MaxLength = maxLength;
+        public int Minimum { get; }
+        public int Maximum { get; }
+
+        public RangeAttribute(int minimum, int maximum)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is int intValue)
+            {
+                return intValue >= Minimum && intValue <= Maximum;
+            }
+
+            return false;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MaxLengthAttribute : ValidationAttribute
+    {
+        public int MaxLength { get; }
+
+        public MaxLengthAttribute(int maxLength)
+        {
+            MaxLength = maxLength;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is string strValue)
+            {
+                return strValue.Length <= MaxLength;
+            }
+
+            return false;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CustomValidationAttribute : ValidationAttribute
+    {
+        private readonly string _customValidationParameter;
+
+        public CustomValidationAttribute(string customValidationParameter)
+        {
+            _customValidationParameter = customValidationParameter;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true; // Validation is successful for null values.
+            }
+
+            string stringValue = value.ToString();
+
+            // Custom validation logic based on the provided parameter.
+            return stringValue.Contains(_customValidationParameter);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PatternAttribute : ValidationAttribute
+    {
+        private readonly string _pattern;
+
+        public PatternAttribute(string pattern)
+        {
+            _pattern = pattern;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true; // Validation is successful for null values.
+            }
+
+            string stringValue = value.ToString();
+
+            // Regular expression validation
+            return System.Text.RegularExpressions.Regex.IsMatch(stringValue, _pattern);
+        }
     }
 }

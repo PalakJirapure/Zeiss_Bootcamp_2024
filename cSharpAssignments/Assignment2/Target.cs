790befdc6012b21a3b9f6bf6b3d501c3ec429921
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 16:06:04 +0530

    Create Target.cs

---
+++ cSharpAssignments/Assignment2/Target.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ObserverPatternExample
+{
+    public class Target
+    {
+        public void ExecuteTask()
+        {
+            Console.WriteLine("Task executed by Target");
+        }
+    }
+}

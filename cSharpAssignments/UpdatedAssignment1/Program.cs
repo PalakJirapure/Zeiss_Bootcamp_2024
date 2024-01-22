630170a7d72a16b0e19f170a22312944555829e3
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 13:06:29 +0530

    Update Program.cs

--- cSharpAssignments/UpdatedAssignment1/Program.cs
+++ cSharpAssignments/UpdatedAssignment1/Program.cs
@@ -1,29 +1,32 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+namespace DeviceValidatorAssignment
 {
-    static void Main()
+    class Program
     {
-        Device deviceObj = new Device();
-        
-        deviceObj.Id = "31027INT";
-        deviceObj.Code = 5;
-        deviceObj.Description = "Good toy to play with";
-        
-        List<string> errors;
-        bool isValid = ObjectValidator.Validate(deviceObj, out errors);
+        static void Main()
+        {
+            Device deviceObj = new Device();
+            deviceObj.Id = "31027INT";
+            deviceObj.Code = 5;
+            deviceObj.Description = "Good toy to play with";
+            deviceObj.CustomProperty = "ABC Custom Value";
+            
+            List<string> errors;
+            bool isValid = ObjectValidator.Validate(deviceObj, out errors);
 
-        if (!isValid)
-        {
-            foreach (string item in errors)
+            if (!isValid)
             {
-                Console.WriteLine(item);
+                foreach (string item in errors)
+                {
+                    Console.WriteLine(item);
+                }
             }
-        }
-        else
-        {
-            Console.WriteLine("All Perfect!");
+            else
+            {
+                Console.WriteLine("All Perfect!");
+            }
         }
     }
 }

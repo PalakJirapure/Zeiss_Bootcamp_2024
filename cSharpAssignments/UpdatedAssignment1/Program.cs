f05c01464f466bbd4d9294f901aef52c073c7892
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 11:28:32 +0530

    Update Program.cs

--- cSharpAssignments/UpdatedAssignment1/Program.cs
+++ cSharpAssignments/UpdatedAssignment1/Program.cs
@@ -6,9 +6,11 @@ class Program
     static void Main()
     {
         Device deviceObj = new Device();
+        
         deviceObj.Id = "31027INT";
         deviceObj.Code = 5;
         deviceObj.Description = "Good toy to play with";
+        
         List<string> errors;
         bool isValid = ObjectValidator.Validate(deviceObj, out errors);
 

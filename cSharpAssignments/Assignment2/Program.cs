f1bc6c0ca9120545c0683f61f49f6046810288a8
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 16:09:31 +0530

    Update Program.cs

--- cSharpAssignments/Assignment2/Program.cs
+++ cSharpAssignments/Assignment2/Program.cs
@@ -6,22 +6,22 @@ namespace ObserverPatternExample
     {
         static void Main()
         {
-            // Create instances of the targets
+
             Target target = new Target();
             Target2 target2 = new Target2();
 
-            // Create instances of the command and source
+            
             Command command = new Command();
             Source source = new Source();
 
-            // Subscribe Command execution to the Source
+           
             source.ExecuteCommand += (task) =>
             {
-                // This handler is optional and can be used to perform additional actions
+                
                 Console.WriteLine("Command received: " + task);
             };
 
-            // Trigger the command, which will execute the tasks in both targets using reflection
+           
             source.TriggerCommand(command, "Task Executed", target, target2);
         }
     }

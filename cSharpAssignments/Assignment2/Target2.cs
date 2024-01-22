76d4c1ddc3dc4b849c5bd456d1050ade8f468578
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 16:06:34 +0530

    Create Target2.cs

---
+++ cSharpAssignments/Assignment2/Target2.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ObserverPatternExample
+{
+    public class Target2
+    {
+        public void DoTask()
+        {
+            Console.WriteLine("Task executed by Target2");
+        }
+    }
+}

d15b63bcb1f0843718463e8dd726a9547ad6f3dc
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-22 11:27:42 +0530

    Update Program.cs

--- cSharpAssignments/UpdatedAssignment1/Program.cs
+++ cSharpAssignments/UpdatedAssignment1/Program.cs
@@ -1,18 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-class Device
-{
-    [Required]
-    public string Id { get; set; }
-
-    [Range(10, 100)]
-    public int Code { get; set; }
-
-    [MaxLength(100)]
-    public string Description { get; set; }
-}
-
 class Program
 {
     static void Main()

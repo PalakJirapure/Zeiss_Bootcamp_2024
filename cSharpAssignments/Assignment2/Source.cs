6b9fa9e298c2c38b8e7fa11a238de07a71b836c9
PalakJirapure <86233386+PalakJirapure@users.noreply.github.com>
2024-01-23 09:47:16 +0530

    Update Source.cs

--- cSharpAssignments/Assignment2/Source.cs
+++ cSharpAssignments/Assignment2/Source.cs
@@ -6,15 +6,15 @@ namespace ObserverPatternExample
 
     public class Source
     {
-        // Event for the command execution
+        
         public event ExecuteCommandDelegate ExecuteCommand;
 
         public void TriggerCommand(Command command, string task, params object[] targets)
         {
-            // Invoke the event, triggering the command execution
+            
             ExecuteCommand?.Invoke(task);
 
-            // Use reflection to execute the tasks in the specified targets
+            
             command.Execute(task, targets);
         }
     }
